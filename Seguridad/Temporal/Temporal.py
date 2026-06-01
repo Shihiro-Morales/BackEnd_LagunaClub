a641b489bf634975be2a580599fdc3abb4aa6c18
Manuel <francisco192lopez143@gmail.com>
2026-06-01 01:25:18 -0600

    Fix admin password

--- Seguridad/Temporal/Temporal.py
+++ Seguridad/Temporal/Temporal.py
@@ -1,17 +1,27 @@
 from django.contrib.auth import get_user_model
+from gunicorn.config import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-User = get_user_model()
 
 @api_view(['POST'])
 def crear_admin(request):
 
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='admin',
-            password='123456',
-            email='admin@gmail.com'
-        )
+    user, created = User.objects.get_or_create(
+        username='admin',
+        defaults={
+            'email': 'admin@gmail.com',
+            'is_staff': True,
+            'is_superuser': True,
+        }
+    )
 
-    return Response({"mensaje": "Admin creado"})
+    user.set_password('123456')
+    user.is_active = True
+    user.save()
+
+    return Response({
+        "created": created,
+        "username": user.username,
+        "is_active": user.is_active
+    })
